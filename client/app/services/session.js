222b978646193fc0129cd0a3e645c44ba02d5ed9
Alex Panov <windstarter@gmail.com>
2017-07-13 00:50:57 +0300

    Devise setup

---
+++ client/app/services/session.js
@@ -0,0 +1,7 @@
+// import Ember from 'ember';
+// import DeviseAuthenticator from 'ember-simple-auth/authenticators/devise';
+import SessionService from 'ember-simple-auth/services/session';
+
+export default SessionService.extend({
+
+});
