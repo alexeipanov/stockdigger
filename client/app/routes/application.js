62864ff8bd3bfa59a708f7d1d57a6772dafef756
Alex Panov <windstarter@gmail.com>
2017-08-25 12:58:49 +0300

    move pagetitle setter to app.route

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+export default Ember.Route.reopen(ApplicationRouteMixin, {
+  actions: {
+    didTransition() {
+      this.controller.set('pageTitle', this.get('pageTitle'));
+      return true;
+    }
+  },
 
 });
