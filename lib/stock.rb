149809b5cf26a56f01599543b9cf0fd3cf553abd
Alex Panov <windstarter@gmail.com>
2017-08-26 18:42:49 +0300

    add search images method

--- lib/stock.rb
+++ lib/stock.rb
@@ -10,10 +10,6 @@ module Stock
       self.base_uri = 'https://api.shutterstock.com/v2/'
       self.client_id = '617f8ecd027040e4c0a9'
       self.client_secret = 'c11b974dd242055c6bd1301a26091c77c4dcb5db'
-      self.options = {
-        view: 'minimal',
-        fields: 'id,assets/large_thumb/url'
-      }
       self.headers = {
         'Authorization' => 'Basic ' + Base64.strict_encode64("#{self.client_id}:#{self.client_secret}"),
         'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'
@@ -21,9 +17,27 @@ module Stock
     end
 
     def get_image(id)
-      response = HTTParty.get("#{self.base_uri}images/#{id}", query: self.options, headers: self.headers)
+      options = {
+        view: 'minimal',
+        fields: 'id,assets/large_thumb/url'
+      }
+      response = HTTParty.get("#{self.base_uri}images/#{id}", query: options, headers: self.headers)
       # TODO Add error handling
       response['assets']['large_thumb']['url']
+    end
+
+    def search(keyword)
+      options = {
+        fields: 'data/id',
+        license: 'commercial',
+        page: 1,
+        per_page: 50,
+        query: keyword,
+        sort: 'popular',
+        view: 'minimal'
+      }
+      response = HTTParty.get("#{self.base_uri}images/search", query: options, headers: self.headers)
+      response['data']
     end
 
   end
--- spec/shutter_spec.rb
+++ spec/shutter_spec.rb
@@ -1,17 +1,22 @@
 require 'stock'
 
 RSpec.describe Stock::Shutterstock do
-  context 'Shutterstock_auth' do
-
+  context 'shutter' do
     it 'initialize check' do
       shutter = Stock::Shutterstock.new
       expect(shutter.headers['Authorization'].chomp).to eq('Basic NjE3ZjhlY2QwMjcwNDBlNGMwYTk6YzExYjk3NGRkMjQyMDU1YzZiZDEzMDFhMjYwOTFjNzdjNGRjYjVkYg==')
     end
 
-    it 'get test image' do
+    it 'get image' do
       shutter = Stock::Shutterstock.new
       uri = shutter.get_image(321982310)
-      expect(uri).to eq 'https://image.shutterstock.com/display_pic_with_logo/3434738/487421065/stock-photo-reggae-colors-flag-crochet-knitted-style-background-top-view-collage-with-mirror-reflection-487421065.jpg'
+      expect(uri).to eq 'https://thumb1.shutterstock.com/thumb_large/1340161/321982310/stock-vector-vector-set-with-music-instruments-for-reggae-style-isolated-music-instruments-for-reggae-with-321982310.jpg'
+    end
+
+    it 'search images' do
+      shutter = Stock::Shutterstock.new
+      result = shutter.search('reggae')
+      expect(result.at(7)['id'].to_i).to eq 487421065
     end
   end
 end
