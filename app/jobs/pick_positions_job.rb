6564bb1933068551317aa1155900022cc32f0fb6
Alex Panov <windstarter@gmail.com>
2017-08-26 20:06:21 +0300

    configure jobs

--- app/jobs/pick_positions_job.rb
+++ app/jobs/pick_positions_job.rb
@@ -1,7 +1,16 @@
 class PickPositionsJob < ApplicationJob
   queue_as :default
+  require 'stock'
 
   def perform(*args)
-    # Do something later
+    shutter = Stock::Shutterstock.new
+    Collection.all.each do |collection|
+      collection.keywords.each do |keyword|
+        result = shutter.search(keyword.keyword)
+        collection.images.each do |image|
+          Position.create({ :image_id => image.id, :keyword_id => keyword_id, :position => result.index(image.image) })
+        end
+      end
+    end
   end
 end
