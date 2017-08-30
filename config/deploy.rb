acb831ecf502b0f66fd3b577440efc830bfeb36a
Alex Panov <windstarter@gmail.com>
2017-08-30 18:44:24 +0300

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,15 @@ lock "3.9.0"
 set :application, "stockDigger"
 set :repo_url, "git@bitbucket.org:windstarter/stockdigger.git"
 
+set :rbenv_ruby, '2.4.1'
+set :rbenv_type, :user
+
+
+
 # Set up puma
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :puma_bind,       "unix:///home/ap/www/stockdigger/puma.sock"
-# set :puma_bind,       "unix:///home/ap/www/stockdigger/tmp/sockets/puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
