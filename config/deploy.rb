d120aae8403f3d9989f43b3d998e67e6d0e09a44
Alex Panov <windstarter@gmail.com>
2017-08-30 11:59:52 +0300

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+# config valid only for current version of Capistrano
+lock "3.9.0"
+
+set :application, "stockDigger"
+set :repo_url, "git@bitbucket.org:windstarter/stockdigger.git"
+
+# Set up puma
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix:///home/ap/www/stockdigger/puma.sock"
+# set :puma_bind,       "unix:///home/ap/www/stockdigger/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/ap/www/stockdigger"
+set :assets_roles, []
+
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+set :keep_releases, 3
