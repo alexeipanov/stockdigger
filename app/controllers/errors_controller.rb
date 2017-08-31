621183f8df3b2781f26c473c87d99d68a7344cd6
Alex Panov <windstarter@gmail.com>
2017-08-31 22:49:20 +0300

    remove errors controller

--- app/controllers/errors_controller.rb
+++
@@ -1,5 +0,0 @@
-class ErrorsController < ActionController::Base
-  def not_found
-  	render(:status => 200)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'errors/not_found'
-
   resources :collections do
   	resources :keywords
   	resources :images
@@ -14,6 +12,4 @@ Rails.application.routes.draw do
   get 'me' => 'users#me'
   get 'profile' => 'profile#show'
   post 'user_token' => 'user_token#create'
-
-  match "/404", :to => "errors#not_found", :via => :all
 end
