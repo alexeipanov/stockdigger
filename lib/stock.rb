630ee422ce91432119203f8a5b1730a1536403e8
Alex Panov <windstarter@gmail.com>
2017-09-05 13:24:18 +0300

    stock module: errors handling

--- lib/stock.rb
+++ lib/stock.rb
@@ -2,18 +2,24 @@ require 'httparty'
 require 'base64'
 
 module Stock
-
   class Shutterstock
-    attr_accessor :headers, :client_id, :client_secret, :base_uri, :options
-
-    def initialize
-      self.base_uri = 'https://api.shutterstock.com/v2/'
-      self.client_id = '617f8ecd027040e4c0a9'
-      self.client_secret = 'c11b974dd242055c6bd1301a26091c77c4dcb5db'
-      self.headers = {
-        'Authorization' => 'Basic ' + Base64.strict_encode64("#{self.client_id}:#{self.client_secret}"),
-        'User-Agent' => 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'
+    attr_reader :headers
+    def initialize(credentials)
+      @base_uri = 'https://api.shutterstock.com/v2/'
+      @client_id = credentials[:client_id]
+      @client_secret = credentials[:client_secret]
+      @headers = {
+        'Authorization' => 'Basic ' + Base64.strict_encode64("#{@client_id}:#{@client_secret}"),
+        'User-Agent' => 'Shutterstock API client'
       }
+    end
+
+    def test_auth
+      options = {
+        id: 1
+      }
+      response = HTTParty.get("#{@base_uri}images", query: options, headers: @headers)
+      response.code
     end
 
     def get_image(id)
@@ -21,9 +27,8 @@ module Stock
         view: 'minimal',
         fields: 'id,assets/large_thumb/url'
       }
-      response = HTTParty.get("#{self.base_uri}images/#{id}", query: options, headers: self.headers)
-      # TODO Add error handling
-      response['assets']['large_thumb']['url']
+      response = HTTParty.get("#{@base_uri}images/#{id}", query: options, headers: @headers)
+      response.code.eql?(200) ? response['assets']['large_thumb']['url'] : ''
     end
 
     def search(keyword)
@@ -36,10 +41,8 @@ module Stock
         sort: 'popular',
         view: 'minimal'
       }
-      response = HTTParty.get("#{self.base_uri}images/search", query: options, headers: self.headers)
-      response['data']
+      response = HTTParty.get("#{@base_uri}images/search", query: options, headers: @headers)
+      response.code.eql?(200) ? response['data'] : []
     end
-
   end
-
 end
--- spec/shutter_spec.rb
+++ spec/shutter_spec.rb
@@ -2,21 +2,26 @@ require 'stock'
 
 RSpec.describe Stock::Shutterstock do
   context 'shutter' do
-    it 'initialize check' do
-      shutter = Stock::Shutterstock.new
-      expect(shutter.headers['Authorization'].chomp).to eq('Basic NjE3ZjhlY2QwMjcwNDBlNGMwYTk6YzExYjk3NGRkMjQyMDU1YzZiZDEzMDFhMjYwOTFjNzdjNGRjYjVkYg==')
+    credentials = { client_id: 'fffe5-175c2-77e16-7c7da-9f85d-70870', client_secret: '401d0-a45c5-3d2c9-1e8c2-5f23b-17a80' }
+    it 'initialize check and basic auth' do
+      shutter = Stock::Shutterstock.new(credentials)
+      expect(shutter.test_auth).to eq(200)
     end
 
     it 'get image' do
-      shutter = Stock::Shutterstock.new
+      shutter = Stock::Shutterstock.new(credentials)
       uri = shutter.get_image(321982310)
       expect(uri).to eq 'https://thumb1.shutterstock.com/thumb_large/1340161/321982310/stock-vector-vector-set-with-music-instruments-for-reggae-style-isolated-music-instruments-for-reggae-with-321982310.jpg'
+      uri = shutter.get_image('321982310ab')
+      expect(uri).to eq ''
     end
 
     it 'search images' do
-      shutter = Stock::Shutterstock.new
+      shutter = Stock::Shutterstock.new(credentials)
       result = shutter.search('reggae')
       expect(result.at(7)['id'].to_i).to eq 487421065
+      result = shutter.search('iuaewrksdnmf')
+      expect(result.empty?).to eq true
     end
   end
 end
