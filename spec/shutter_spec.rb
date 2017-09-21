4973bd5248aba8a0f39d6d6bd9bf803dfea48496
Alexei Panov <windstarter@gmail.com>
2017-09-21 20:32:47 +0300

    simplify shutterstock test case

--- spec/shutter_spec.rb
+++ spec/shutter_spec.rb
@@ -3,13 +3,14 @@ require 'stock'
 RSpec.describe Stock::Shutterstock do
   context 'shutter' do
     credentials = { client_id: 'fffe5-175c2-77e16-7c7da-9f85d-70870', client_secret: '401d0-a45c5-3d2c9-1e8c2-5f23b-17a80' }
+    shutter = Stock::Shutterstock.new(credentials)
     it 'initialize check and basic auth' do
-      shutter = Stock::Shutterstock.new(credentials)
+
       expect(shutter.test_auth).to eq(200)
     end
 
     it 'get image' do
-      shutter = Stock::Shutterstock.new(credentials)
+      # shutter = Stock::Shutterstock.new(credentials)
       uri = shutter.get_image(321982310)
       expect(uri).to eq 'https://thumb1.shutterstock.com/thumb_large/1340161/321982310/stock-vector-vector-set-with-music-instruments-for-reggae-style-isolated-music-instruments-for-reggae-with-321982310.jpg'
       uri = shutter.get_image('321982310ab')
@@ -17,7 +18,7 @@ RSpec.describe Stock::Shutterstock do
     end
 
     it 'search images' do
-      shutter = Stock::Shutterstock.new(credentials)
+      # shutter = Stock::Shutterstock.new(credentials)
       result = shutter.search('reggae')
       expect(result.at(7)['id'].to_i).to eq 492046210
       result = shutter.search('iuaewrksdnmf')
