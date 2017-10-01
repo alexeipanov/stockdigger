1961b0aaebd52c0d0e38cce7b5668528aa45dc13
Alexei Panov <windstarter@gmail.com>
2017-10-01 13:08:52 +0300

    add ember model test

--- client/tests/unit/models/user-test.js
+++ client/tests/unit/models/user-test.js
@@ -1,12 +1,18 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('user', 'Unit | Model | user', {
-  // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:image']
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
-  // let store = this.store();
   assert.ok(!!model);
+});
+
+test('contains images', function(assert) {
+  let user = this.store().modelFor('user');
+  const relationship = Ember.get(user, 'relationshipsByName').get('images');
+  assert.equal(relationship.key, 'images', 'has relationship with image');
+  assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
 });
